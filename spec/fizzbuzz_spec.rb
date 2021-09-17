d3d60a9aed7830d07c1aa2dffd8a55a7bc2d12a2
serenabertozzi <serenabertozzi@protonmail.com>
2021-09-17 13:21:17 +0100

    write and pass final test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -14,7 +14,7 @@ describe "#fizzbuzz" do
   end
 
   it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
 
   it 'return "fizz" when passed multiples of 3 that are not multiples of 5' do
@@ -23,6 +23,33 @@ describe "#fizzbuzz" do
 
     multiples_of_three.each { |num| result << fizzbuzz(num) }
 
-    expect(result).to eq Array.new(multiples_of_three.length, 'fizz')
+    expect(result).to eq Array.new(multiples_of_three.length, "fizz")
+  end
+
+  it 'return "buzz" when passed multiples of 5 that are not multiples of 3' do
+    multiples_of_five = [5, 10, 20, 25, 35, 40]
+    result = []
+
+    multiples_of_five.each { |num| result << fizzbuzz(num) }
+
+    expect(result).to eq Array.new(multiples_of_five.length, "buzz")
+  end
+
+  it 'return "fizzbuzz" when passed multiples of 3 and 5' do
+    multiples_of_three_and_five = [15, 30, 45, 60, 75, 90]
+    result = []
+
+    multiples_of_three_and_five.each { |num| result << fizzbuzz(num) }
+
+    expect(result).to eq Array.new(multiples_of_three_and_five.length, "fizzbuzz")
+  end
+
+  it "return number passed when number is not a multiple of 3 or 5" do
+    non_multiple = [1, 2, 4, 8, 11, 13]
+    result = []
+
+    non_multiple.each { |num| result << fizzbuzz(num) }
+
+    expect(result).to eq non_multiple
   end
 end
